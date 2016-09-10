fefe5700ffe12b5546ba0543dfb74fcc01907992
leobenessuti <leobenessuti@hotmail.com>
2016-09-09 21:51:10 -0300

    first commit

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,37 @@
+$('.collection')
+	.on('click', '.collection-item', function(){
+		var nomeProduto = this.firstChild.textContent;
+		Materialize.toast(nomeProduto + ' adicionado', 1000);
+
+		var $badge = $('.badge', this);
+		if ($badge.length == 0) {
+			$badge = $('<span class="badge browntext">0</span>').appendTo(this);
+		}
+		$badge.text(parseInt($badge.text()) + 1);
+
+
+	})
+
+$('.modal-trigger').leanModal();
+
+$('.collection')
+	.on('click', '.badge', function() {
+		$(this).remove();
+		return false;
+ 	});
+
+$('#confirmar').on('click', function(){
+	var texto = "";
+
+	$('.badge').parent().each(function(){
+		texto += this.firstChild.textContent + ': ';
+		texto += this.lastChild.textContent + '; ';
+	})
+
+	$('#resumo').empty().text(texto);
+});
+
+$('.acao-limpar').on('click', function() {
+	$('#numero-mesa').val('');
+	$('.badge').remove();
+});
